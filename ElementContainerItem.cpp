21cad5cb87f357a2e2ff1a251765c880387f7e00
Metalhead33 <horvathjeno1944@msn.com>
2016-12-28 00:45:14 +0100

    Converted over to QAbstractItemModel.

---
+++ ElementContainerItem.cpp
@@ -0,0 +1,6 @@
+#include "ElementContainerItem.h"
+
+ElementContainerItem::ElementContainerItem()
+{
+
+}
---
+++ ElementContainerItem.hpp
@@ -0,0 +1,11 @@
+#ifndef ELEMENTCONTAINERITEM_H
+#define ELEMENTCONTAINERITEM_H
+
+
+class ElementContainerItem
+{
+public:
+    ElementContainerItem();
+};
+
+#endif // ELEMENTCONTAINERITEM_H
