3ed61530d81b84625fc60fc88c21f2956e5e639b
Metalhead33 <horvathjeno1944@msn.com>
2016-12-28 23:59:56 +0100

    For the very first time, structure files can be created in this program.

--- main.cpp
+++ main.cpp
@@ -10,9 +10,15 @@ Directory* ROOT_DIRECTORY;
 int main(int argc, char *argv[])
 {
     Structure::ROOT_DIRECTORY = new Structure::Directory("root",0);
-    FILE* failor = fopen("/home/metalhead33/testfile2","rb");
+    if(argc > 1) {
+    FILE* failor = fopen(argv[1],"rb");
     Structure::Io::Unserialize(failor);
     fclose(failor);
+    }
+    else
+    {
+        new Structure::Directory("Dir1",Structure::ROOT_DIRECTORY);
+    }
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
